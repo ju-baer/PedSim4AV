8811855c12c7acf731d052166a40c7ec9305f645
S.M. Jubaer <108940924+ju-baer@users.noreply.github.com>
2025-08-04 21:53:17 +0600

    Delete models/common directory

--- models/common/data_processing.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def load_trajectory_data(file_path):
-    """Load trajectory data from CSV."""
-    df = pd.read_csv(file_path, names=['ped_id', 'frame', 'x', 'y'])
-    return df.groupby('ped_id').apply(lambda x: x[['x', 'y']].values).to_dict()
